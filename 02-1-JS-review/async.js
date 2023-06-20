2c6fca9b783a851a89186576af26150cd42d83ce
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-06-20 06:37:18 +0500

    Add async-await

--- 02-1-JS-review/async.js
+++ 02-1-JS-review/async.js
@@ -13,8 +13,12 @@ async function getUsers() {
   const res = await fetch('https://jsonplaceholder.typicode.com/users');
   const data = await res.json();
   console.log('data: ', data );
+
+  return data;
 }
 
-getUsers();
+// getUsers();
+const users = getUsers();
+console.log(': ', users);  // Promise {<pending>}
 
 console.log('users: ');
