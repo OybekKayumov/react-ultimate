bb3dd32109b39a6a7a1d6494388032bc53bb67cc
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-06-21 13:46:17 +0500

    Add React Fragments

--- 03-pizza-menu/src/components/Menu.js
+++ 03-pizza-menu/src/components/Menu.js
@@ -9,7 +9,7 @@ const Menu = () => {
 
   return (
     <main className='menu'>
-      <h2>Our menu</h2>
+      <h2>Our menu</h2>      
 
       {/*  
       { numPizzas > 0 && (
@@ -19,9 +19,16 @@ const Menu = () => {
       )}
       */}
       { numPizzas > 0 ? (
-        <ul className='pizzas'>
-        {pizzas.map(pizza => <Pizza pizzaObj={pizza} key={pizza.name}/>)}
-      </ul>
+        <>
+          <p>
+            Authentic Italian cuisine. 6 creative dishes to choose from. All
+            from our stone oven, all organic, all delicious.
+          </p>
+
+          <ul className='pizzas'>
+            {pizzas.map(pizza => <Pizza pizzaObj={pizza} key={pizza.name}/>)}
+          </ul>
+        </>
       ) : (
         // null
         <p>We're still working on our menu. Please come back later :)</p>
