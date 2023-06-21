b45b8baa85e5965e2f8f6e77ae392324119a85ef
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-06-21 11:56:39 +0500

    Add Conditional Rendering With &&

--- 03-pizza-menu/src/components/Menu.js
+++ 03-pizza-menu/src/components/Menu.js
@@ -3,14 +3,20 @@ import Pizza from './Pizza'
 import {pizzaData} from './../data/data';
 
 const Menu = () => {
+  // const pizzas = pizzaData;
+  const pizzas = [];
+  const numPizzas = pizzas.length;
+
   return (
     <main className='menu'>
       <h2>Our menu</h2>
-
-      <ul className='pizzas'>
+      { pizzas && (
+        <ul className='pizzas'>
         {/* {pizzaData.map(pizza => <Pizza name={pizza.name}/>)} */}
-        {pizzaData.map(pizza => <Pizza pizzaObj={pizza} key={pizza.name}/>)}
+        {/* {pizzaData.map(pizza => <Pizza pizzaObj={pizza} key={pizza.name}/>)} */}
+        {pizzas.map(pizza => <Pizza pizzaObj={pizza} key={pizza.name}/>)}
       </ul>
+      )}
 
       {/*
       <Pizza 
