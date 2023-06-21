4c0ed85a982ec191e640a792cb9014ebda1d8123
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-06-21 05:07:44 +0500

    Add props

--- 03-pizza-menu/src/components/Menu.js
+++ 03-pizza-menu/src/components/Menu.js
@@ -5,9 +5,21 @@ const Menu = () => {
   return (
     <main className='menu'>
       <h2>Our menu</h2>
-      <Pizza />
+      <Pizza 
+        name='Pizza Spinaci' 
+        ingredients='tomato,...'
+        photoName='pizzas/spinaci.jpg'
+        price='10'
+      />
+
+      <Pizza 
+        name='Pizza Funghi' 
+        ingredients='mushrooms,...'
+        photoName='pizzas/funghi.jpg'
+        price='12'
+      />
     </main>
   )
 }
 
-export default Menu
+export default Menu;
--- 03-pizza-menu/src/components/Pizza.js
+++ 03-pizza-menu/src/components/Pizza.js
@@ -1,7 +1,15 @@
 // import data from './../../public/data';
+import Menu from "./Menu"
 
-export default function Pizza(params) {
+export default function Pizza(props) {
+  console.log('props: ', props );
+
   return (
-    <h3>Pizza</h3> 
+    <div>
+      <img src={props.photoName} alt={props.name} />
+      <h3>{props.name}</h3>
+      <p>{props.ingredients}</p>
+      <p>{props.price}</p>
+    </div>
   )
 }
