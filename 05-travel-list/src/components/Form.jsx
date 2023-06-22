b5d4736235b52496246e3a271664b3daea8f9b16
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-06-22 13:39:49 +0500

    AddBuilding a Form and Handling Submissions

--- 05-travel-list/src/components/Form.jsx
+++ 05-travel-list/src/components/Form.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
 
 const Form = () => {
+  function handleSubmit(e) {
+    e.preventDefault();
+    
+  }
+
   return (
-    <form className='add-form'>
+    <form className='add-form' onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
 
       <select name="" id="">
-        <option value={1}>1</option>
-        <option value={2}>2</option>
-        <option value={3}>3</option>
+        {Array.from({ length: 20}, (_, i) => i+1).map(
+          num => (
+            <option value={num} key={num}>
+              {num}
+            </option>
+          ))}
       </select>
       <input type="text" placeholder='Item...' />
       <button>Add</button>
