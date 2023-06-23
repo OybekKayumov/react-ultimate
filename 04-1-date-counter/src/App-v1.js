aa366eedeb6d51e5ac67a34f7a1802847248b3c0
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-06-23 05:42:34 +0500

    Add challenge 2: Date Counter v2

--- 04-1-date-counter/src/App-v1.js
+++ 04-1-date-counter/src/App-v1.js
@@ -16,30 +16,17 @@ function Counter() {
   const date = new Date();
   date.setDate(date.getDate() + count);
 
-  function handleReset() {
-    setCount(0);
-    setStep(1);
-  }
-
   return (
     <div>
       <div>
-        <input 
-          type="range" 
-          min="0" max="10" 
-          value={step} 
-          onChange={(e) => setStep(Number(e.target.value))}
-        />
-
+        <button onClick={() => setStep(c => c -1)}>-</button>
         <span>Step: {step}</span>
+        <button onClick={() => setStep(c => c +1)}>+</button>
       </div>
 
       <div>
         <button onClick={() => setCount(c => c -step)}>-</button>
-        <input type="text"
-          value={count}
-          onChange={(e) => setCount(Number(e.target.value))}
-        />
+        <span>Count: {count}</span>
         <button onClick={() => setCount(c => c +step)}>+</button>
       </div>
 
@@ -54,14 +41,6 @@ function Counter() {
         </span>
         <span>{date.toDateString()}</span>
       </p>
-
-      {(count !== 0 || step !== 1) 
-        ? (<div>
-            <button onClick={handleReset}>Reset</button>
-          </div>
-        )
-        : null
-      }
     </div>
   )
 }
