874ce76759e5624b46295135a5d5577422065985
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-06-24 11:50:17 +0500

    Add accordion component

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -38,7 +38,11 @@ function App() {
   }
 
   function handleClearList() {
-    setItems([]);
+    const confirmed = window.confirm(
+      'Are you sure you want to delete all items?'
+    );
+
+    if (confirmed) setItems([]);
   }
   
   return (
