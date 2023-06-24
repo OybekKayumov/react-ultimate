2d7541ac072322ea4c2e27a5fc354ec5fac7ee4d
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-06-24 12:08:43 +0500

    Add The children Prop: Making a Reusable Button

--- 04-steps/src/App-v1.js
+++ 04-steps/src/App-v1.js
@@ -11,6 +11,7 @@ export default function App() {
   return (
     <div>
       <Steps />
+      <Steps />
     </div>
   )
 }
@@ -62,39 +63,24 @@ function Steps() {
           </p>
 
           <div className="buttons">
-            <Button 
-              bgColor='#7950f2' 
-              textColor="#fff"
-              text='Prev'
-              emoji='◀'
+            <button 
+              style={{backgroundColor: '#7950f2', color: "#fff" }}
               onClick={handlePrev}
+              // onMouseEnter={alert("123")}  //!
             >
-            </Button>
-
-            <Button 
-              bgColor='#7950f2' 
-              textColor="#fff"
-              text='Next'
-              emoji='▶'
+              Previous
+            </button>
+            <button 
+              style={{backgroundColor: '#7950f2', color: "#fff" }}
               onClick={handleNext}
             >
-            </Button>
+              Next
+            </button>
           </div>
         </div>
       )}
     </div>
   );
-}
-
-function Button({ textColor, bgColor, onClick, text, emoji }) {
-  return (
-    <button 
-      style={{backgroundColor: bgColor, color: textColor }}
-      onClick={onClick}
-    >
-      <span>{emoji}</span> {text}
-    </button>
-  )
 }
 
 // export default App;
