7c1624dbff9ccddb02a542f1dd95301d57f56148
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-06-24 14:16:19 +0500

    Add More Reusability With the children Prop

--- 05-2-accordion-component/src/App-v1.js
+++ 05-2-accordion-component/src/App-v1.js
@@ -27,46 +27,20 @@ function App() {
 }
 
 function Accordion({ data }) {
-  const [curOpen, setCurOpen] = useState(null);
-
   return (
     <div className="accordion">
       {data.map((el, i) => (
-        <AccordionItem 
-          curOpen={curOpen} 
-          onOpen={setCurOpen}
-          title={el.title} num={i} key={el.title} 
-        >
-        {el.text}
-        </AccordionItem>
+        <AccordionItem title={el.title} text={el.text} num={i} key={el.title} />
       ))}
-
-      <AccordionItem
-        curOpen={curOpen}
-        onOpen={setCurOpen}
-        title="Test 1"
-        num={20}
-        key="test 1"
-      >
-        <p>Allows React developers to:</p>
-        <ul>
-          <li>Break up UI into components</li>
-          <li>Make components reusuable</li>
-          <li>Place state efficiently</li>
-        </ul>
-      </AccordionItem>
     </div>
   );
 }
 
-function AccordionItem({ num, title, curOpen, onOpen, children }) {
-  // const [isOpen, setIsOpen] = useState(false);
-  const isOpen = num === curOpen;
+function AccordionItem({ num, title, text }) {
+  const [isOpen, setIsOpen] = useState(false);
 
   function handleToggle() {
-    // setIsOpen(isOpen => !isOpen);
-    onOpen(isOpen ? null : num);
-
+    setIsOpen(isOpen => !isOpen);
   }
 
   return (
@@ -77,7 +51,7 @@ function AccordionItem({ num, title, curOpen, onOpen, children }) {
 
       { isOpen && 
         <div className="content-box">
-          {children}
+          {text}
         </div>
       }
     </div>
