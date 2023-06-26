d97a9a6121c20a78173e4dc2e66683817762eafe
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-06-26 06:47:45 +0500

    Add Building the Static App: List of Friends

--- 06-eat-n-split/src/App.js
+++ 06-eat-n-split/src/App.js
@@ -4,18 +4,21 @@ const initialFriends = [
     name: "Clark",
     image: "https://i.pravatar.cc/48?u=118836",
     balance: -7,
+    currency: "$",
   },
   {
     id: 933372,
     name: "Sarah",
     image: "https://i.pravatar.cc/48?u=933372",
     balance: 20,
+    currency: "€",
   },
   {
     id: 499476,
     name: "Anthony",
     image: "https://i.pravatar.cc/48?u=499476",
     balance: 0,
+    currency: "€",
   },
 ];
 
@@ -43,7 +46,26 @@ function FriendsList() {
 
 function Friend({ friend }) {
   return (
-    <li>{friend.name}</li>
+    <li>
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
+
+      { friend.balance < 0 && (
+        <p className='red'>
+          You owe {friend.name} {Math.abs(friend.balance)}{friend.currency}.
+        </p>
+      )}
+      { friend.balance > 0 && (
+        <p className='green'>
+          {friend.name} owes you {Math.abs(friend.balance)}{friend.currency}.
+        </p>
+      )}
+      { friend.balance === 0 && (
+        <p>
+          You and {friend.name} are even
+        </p>
+      )}
+    </li>
   )
 }
 
