35e560d2008890b8bb36b91f180dcc78f9666493
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-06-26 06:03:33 +0500

    Add tip calculator

--- 05-3-tip-calc/src/App.js
+++ 05-3-tip-calc/src/App.js
@@ -52,7 +52,7 @@ function TipCalculator() {
 function BillInput({ bill, onSetBill }) {
   return (
     <div>
-      <label htmlFor="">How much was the bill?</label>
+      <label>How much was the bill?</label>
       <input 
         type="text" 
         placeholder='Bill value' 
@@ -66,16 +66,16 @@ function BillInput({ bill, onSetBill }) {
 function SelectPercentage({ children, percentage, onSelect }) {
   return (
     <div>
-      <label htmlFor="">{children}</label>
+      <label>{children}</label>
 
       <select 
         value={percentage} 
         onChange={e => onSelect(Number(e.target.value))}
       >
         <option value="0">Dissatisfied (0%)</option>
-        <option value="0">It was OK (5%)</option>
-        <option value="0">It was good (10%)</option>
-        <option value="0">Absolutely amazing (20%)</option>
+        <option value="5">It was OK (5%)</option>
+        <option value="10">It was good (10%)</option>
+        <option value="20">Absolutely amazing (20%)</option>
       </select>
     </div>
   )
