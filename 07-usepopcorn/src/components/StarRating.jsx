e58e9b155d641975529c16f3bac253198c2206e1
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-06-28 15:00:36 +0500

    Add Building a Reusable Star Rating Component

--- 07-usepopcorn/src/components/StarRating.jsx
+++ 07-usepopcorn/src/components/StarRating.jsx
@@ -1,16 +1,33 @@
 import React from 'react'
 
-const StarRating = () => {
-  return (
-    <div>
-      <div>
+const containerStyle = {
+  display: 'flex',
+  alignItems: 'center',
+  gap: '16px',
+}
 
-        {Array.from({ length: 5}, (_, i) => 
+const starContainerStyle = {
+  display: 'flex',
+  // alignItems: 'center',
+  gap: '4px',
+}
+
+const textStyle = {
+  lineHeight: '1',
+  margin: '0',
+}
+
+const StarRating = ({ maxRating }) => {
+
+  return (
+    <div style={containerStyle}>
+      <div style={starContainerStyle}>
+        {Array.from({ length: maxRating }, (_, i) => 
           <span>S{i + 1}</span>  // i = 0
         )}
       </div>
 
-      <p>10</p>
+      <p style={textStyle}>10</p>
     </div>
   )
 }
