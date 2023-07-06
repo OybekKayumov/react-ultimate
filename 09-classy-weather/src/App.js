bc898db5d75f533b93a6c9a3fa770c097e363e26
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-07-06 05:50:13 +0500

    Add Our First Class Component

--- 09-classy-weather/src/App.js
+++ 09-classy-weather/src/App.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
 class Counter extends React.Component {
-  
+  constructor(props) {
+    super(props)
+    this.state = {count: 50}
+  }
+
+  render() { 
+    return (
+      <div>
+        <button>-</button>
+        <span>0</span>
+        <button>+</button>
+      </div>
+    )
+  }
 }
 
 export default Counter;
