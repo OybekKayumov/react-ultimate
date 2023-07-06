f06b7708866f116359226ee2d9f8c4d2c852a743
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-07-06 14:18:20 +0500

    Add Working With Event Handlers

--- 09-classy-weather/src/App.js
+++ 09-classy-weather/src/App.js
@@ -2,15 +2,23 @@ import React from 'react';
 
 class Counter extends React.Component {
   constructor(props) {
-    super(props)
-    this.state = {count: 50}
+    super(props);
+    this.state = {count: 50};
+    this.handleDecrement = this.handleDecrement.bind(this);
+  }
+
+  handleDecrement() {
+    console.log('this: ', this);
+    this.setState(curState => {
+      return {count: curState.count - 1}
+    })
   }
 
   render() { 
     return (
       <div>
-        <button>-</button>
-        <span>0</span>
+        <button onClick={this.handleDecrement} >-</button>
+        <span>{this.state.count}</span>
         <button>+</button>
       </div>
     )
