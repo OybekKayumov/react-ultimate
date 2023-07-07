6717cb697b9929679146b247727489799a7b6649
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-07-07 14:49:35 +0500

    Add Yet Another Hook: useReducer

--- 10-react-quiz/src/components/DateCounter.js
+++ 10-react-quiz/src/components/DateCounter.js
@@ -4,7 +4,10 @@ import { useReducer, useState } from "react";
 function reducer(state, action) {
   console.log('state, action: ', state, action);
 
-  return state + action
+  // return state + action;
+  if (action.type === 'inc') return state + action.payload;
+  if (action.type === 'dec') return state - action.payload;
+  if (action.type === 'setCount') return action.payload;
 }
 
 function DateCounter() {
@@ -23,18 +26,22 @@ function DateCounter() {
     // setCount((count) => count - 1);
     // setCount((count) => count - step);
 
-    dispatch(-1)
+    // dispatch(-1);
+    dispatch({ type: 'dec', payload: -1 });
   };
   
   const inc = function () {
     // setCount((count) => count + 1);
     // setCount((count) => count + step);
 
-    dispatch(1)
+    // dispatch(1);
+    dispatch({ type: 'inc', payload: 1 });
   };
 
   const defineCount = function (e) {
     // setCount(Number(e.target.value));
+    // dispatch(Number(e.target.value));
+    dispatch({ type: 'setCount', payload: Number(e.target.value) });
   };
 
   const defineStep = function (e) {
