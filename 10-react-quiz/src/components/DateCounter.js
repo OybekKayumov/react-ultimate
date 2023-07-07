6fd3192e3a83a59039103b9bee690d579eb5e4e2
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-07-07 15:39:19 +0500

    Add Managing Related Pieces of State

--- 10-react-quiz/src/components/DateCounter.js
+++ 10-react-quiz/src/components/DateCounter.js
@@ -14,11 +14,17 @@ function reducer(state, action) {
 
 function DateCounter() {
   // const [count, setCount] = useState(0);
+  // const [step, setStep] = useState(1);
 
   // reducer
-  const [count, dispatch] = useReducer(reducer, 0);
+  // const [count, dispatch] = useReducer(reducer, 0);
+  const initialState = {
+    count: 0,
+    step: 1,
+  }
 
-  const [step, setStep] = useState(1);
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const {count, step} = state;  // destructuring from state
 
   // This mutates the date object.
   const date = new Date("june 21 2027");
