aac6b55dfa7b8d34d59aece7bbdbae062472a2d4
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-07-08 05:47:53 +0500

    Add Loading Questions from a Fake API

--- 10-react-quiz/src/App.js
+++ 10-react-quiz/src/App.js
@@ -1,8 +1,18 @@
 // import DateCounter from "./components/DateCounter";
+import { useEffect } from "react";
 import Header from "./components/Header";
 import Main from "./components/Main";
 
 function App() {
+
+  // fetch data from questions.json
+  useEffect(function (params) {
+    fetch('http://localhost:8000/questions')
+      .then(res => res.json())  // response we need to convert to json
+      .then(data => console.log(': ', data))
+      .catch(err => console.error('Error: '))
+  }, [])
+
   return (
     <div className="app">
       {/* <DateCounter /> */}
--- README.md
+++ README.md
@@ -4,3 +4,10 @@
 ### run quokka
        "Ctrl+Shift+P"
        Quokka.js: Start on Current File
+
+### 10-react-quiz
+       - npm i json-server
+       - create "data" folder with questions.json
+       - package.json add "server": "json-server --watch src/data/questions.json --port 8000"
+       - npm run server
+
