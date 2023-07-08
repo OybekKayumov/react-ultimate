7ea3d1f711bf5b110101e79a66479a62ca357fe2
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-07-08 14:13:30 +0500

    Add Loading Questions from a Fake API

--- 10-react-quiz/src/App.js
+++ 10-react-quiz/src/App.js
@@ -1,16 +1,40 @@
 // import DateCounter from "./components/DateCounter";
-import { useEffect } from "react";
+import { useEffect, useReducer } from "react";
 import Header from "./components/Header";
 import Main from "./components/Main";
 
+const initialState = {
+  questions: [],
+
+  // states: loading, error, ready, active, finished
+  status: 'loading',
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'dataReceived': 
+      return {
+        ...state, // all current state
+        questions: action.payload, // set questions to the data we received
+        status: 'ready'
+      }
+      
+    default:
+      break;
+  }
+}
+
 function App() {
+  // reducer
+  const [state, dispatch] = useReducer(reducer, initialState)
 
   // fetch data from questions.json
   useEffect(function (params) {
     fetch('http://localhost:8000/questions')
       .then(res => res.json())  // response we need to convert to json
-      .then(data => console.log(': ', data))
-      .catch(err => console.error('Error: '))
+      // .then(data => console.log(': ', data))
+      .then(data => dispatch({type: 'dataReceived', payload: data}))
+      .catch(err => console.error('Error'))
   }, [])
 
   return (
