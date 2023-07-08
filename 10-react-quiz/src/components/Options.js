ecfba1e8f13762481061a06ce2b0e131ac57b0d8
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-07-08 16:34:12 +0500

    Add Handling New Answers

--- 10-react-quiz/src/components/Options.js
+++ 10-react-quiz/src/components/Options.js
@@ -1,13 +1,24 @@
 import React from 'react'
 
 const Options = ({ question, dispatch, answer }) => {
+  const hasAnswered = answer !== null;
+
   return (
     <div className='options'>
-      {question.options.map((option, ind) => (
+      {question.options.map((option, index) => (
         <button 
-          className='btn btn-options'
+          className={`btn btn-option
+            ${index === answer ? 'answer' : ""}
+            ${hasAnswered 
+              ? index === question.correctOption 
+                ? "correct" 
+                : "wrong"
+              : ""
+            }
+          `}
           key={option}
-          onClick={() =dispatch({type: 'newAnswer', payload: ind})}
+          disabled={hasAnswered}
+          onClick={() => dispatch({type: 'newAnswer', payload: index})}
         >
           {option}
         </button>
