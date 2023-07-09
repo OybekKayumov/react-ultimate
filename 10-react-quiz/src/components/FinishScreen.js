a745cb0ae54805bc748e7a480d8b3f5d9e81420d
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-07-09 05:27:56 +0500

    Add Finishing a Quiz

---
+++ 10-react-quiz/src/components/FinishScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const FinishScreen = ({ points, maxPossiblePoints }) => {
+  const percentage = (points / maxPossiblePoints) * 100;
+  return (
+    <p>
+      You scored <strong>{points}</strong> out of {maxPossiblePoints} ({Math.ceil(percentage)})
+    </p>
+  )
+}
+
+export default FinishScreen
