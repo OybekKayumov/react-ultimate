36befe337d1385d027bdcc640576e2a582421583
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-07-10 19:28:03 +0500

    Add Implementing Main Pages and Routes

--- 11-worldwise/src/App.jsx
+++ 11-worldwise/src/App.jsx
@@ -1,13 +1,19 @@
-// import { useState } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css'
+import Product from './pages/Product';
+import Pricing from './pages/Pricing';
+import Home from './pages/Home';
 
 function App() {
-  // const [count, setCount] = useState(0)
 
   return (
-    <div>
-      WorldWise
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='product' element={<Product />} />
+        <Route path='pricing' element={<Pricing />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
