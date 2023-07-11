5f8e3cd706841d4f32039a643b361251908ae594
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-07-11 19:27:17 +0500

    Add Implementing the Cities List

--- 11-worldwise/src/components/CityList.jsx
+++ 11-worldwise/src/components/CityList.jsx
@@ -1,9 +1,12 @@
 import Spinner from './Spinner';
 import styles from './CityList.module.css';
 import CityItem from './CityItem';
+import Message from './Message';
 
 const CityList = ({ cities, isLoading }) => {
   if (isLoading) return <Spinner />;
+
+  if (!cities.length) return <Message message='Add your city by clicking on a city on the map' />
 
   return (
     <ul className={styles.cityList}>
