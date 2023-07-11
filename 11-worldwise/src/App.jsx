bc61423a6e253b64d6760c8fdbee2ba2799ecf5c
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-07-12 04:44:18 +0500

    Add Implementing the Countries List

--- 11-worldwise/src/App.jsx
+++ 11-worldwise/src/App.jsx
@@ -8,6 +8,7 @@ import AppLayout from './pages/AppLayout';
 import Login from './pages/Login';
 import CityList from './components/CityList';
 import { useEffect, useState } from 'react';
+import CountryList from './components/CountryList';
 
 const BASE_URL = `http://localhost:8000`;
 
@@ -53,7 +54,10 @@ function App() {
               element={<CityList cities={cities} isLoading={isLoading} />}
             />
 
-            <Route path='countries' element={<p>Countries</p>}/>
+            <Route 
+              path='countries' 
+              element={<CountryList cities={cities} isLoading={isLoading}/>}
+            />
             <Route path='form' element={<p>Form</p>}/>
           </Route>
           <Route path='*' element={<PageNotFound />} />
