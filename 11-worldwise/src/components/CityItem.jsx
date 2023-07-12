3e8cbaa1dc3ea07d89642d09e2d6dde80e910498
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-07-12 05:09:49 +0500

    Add Dynamic Routes With URL Parameters

--- 11-worldwise/src/components/CityItem.jsx
+++ 11-worldwise/src/components/CityItem.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import styles from './CityItem.module.css';
 
 const formatDate = (date) =>
@@ -9,15 +10,17 @@ const formatDate = (date) =>
 
 const CityItem = ({ city }) => {
   console.log('city: ', city);
-  const { cityName, emoji, date } = city;
+  const { cityName, emoji, date, id } = city;
 
   return (
-    <li className={styles.cityItem}>
-      <span className={styles.emoji}>{emoji}</span>
-      <h3 className={styles.name}>{cityName}</h3>
-      <time className={styles.date}>({formatDate(date)})</time>
+    <li>
+      <Link  className={styles.cityItem} to={`${id}`}>
+        <span className={styles.emoji}>{emoji}</span>
+        <h3 className={styles.name}>{cityName}</h3>
+        <time className={styles.date}>({formatDate(date)})</time>
 
-      <button className={styles.deleteBtn}>&times;</button>
+        <button className={styles.deleteBtn}>&times;</button>
+      </Link>
     </li>
   )
 }
