28f64d83a5827dc9b20f374f9a42f42581015696
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-07-12 05:13:32 +0500

    Add Dynamic Routes With URL Parameters

--- 11-worldwise/src/components/City.jsx
+++ 11-worldwise/src/components/City.jsx
@@ -1,3 +1,4 @@
+import { useParams } from "react-router-dom";
 import styles from "./City.module.css";
 
 const formatDate = (date) =>
@@ -9,6 +10,9 @@ const formatDate = (date) =>
   }).format(new Date(date));
 
 function City() {
+  const x = useParams();
+  console.log('x: ', x);
+  
   // TEMP DATA
   const currentCity = {
     cityName: "Lisbon",
