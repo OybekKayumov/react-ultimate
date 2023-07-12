b440c3340d7ba5cae059e4d7eb8de1146e4465f3
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-07-12 05:06:41 +0500

    Add Dynamic Routes With URL Parameters

--- 11-worldwise/src/App.jsx
+++ 11-worldwise/src/App.jsx
@@ -10,6 +10,7 @@ import AppLayout from './pages/AppLayout';
 import Login from './pages/Login';
 import CityList from './components/CityList';
 import CountryList from './components/CountryList';
+import City from './components/City';
 
 const BASE_URL = `http://localhost:8000`;
 
@@ -55,6 +56,7 @@ function App() {
               element={<CityList cities={cities} isLoading={isLoading} />}
             />
 
+            <Route path='cities/:id' element={<City />} />
             <Route 
               path='countries' 
               element={<CountryList cities={cities} isLoading={isLoading}/>}
