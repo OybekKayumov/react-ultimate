79e138c103ef85de3257f1e62142874a92bbb778
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-07-12 14:58:36 +0500

    Add Consuming the Context

--- 12-atomic-blog/src/App.js
+++ 12-atomic-blog/src/App.js
@@ -67,8 +67,10 @@ function App() {
           // searchQuery={searchQuery}
           // setSearchQuery={setSearchQuery}
         />
-        <Main posts={searchedPosts} onAddPost={handleAddPost} />
-        <Archive onAddPost={handleAddPost} />
+        {/* <Main posts={searchedPosts} onAddPost={handleAddPost} /> */}
+        {/* <Archive onAddPost={handleAddPost} /> */}
+        <Main />
+        <Archive />
         <Footer />
       </section>
     </PostContext.Provider>
@@ -116,28 +118,36 @@ function SearchPosts() {
 // function Results({ posts }) {
 function Results() {
   const {posts} = useContext(PostContext);
-  
+
   return <p>🚀 {posts.length} atomic posts found</p>;
 }
 
-function Main({ posts, onAddPost }) {
+// function Main({ posts, onAddPost }) {
+function Main() {
   return (
     <main>
-      <FormAddPost onAddPost={onAddPost} />
-      <Posts posts={posts} />
+      {/* <FormAddPost onAddPost={onAddPost} /> */}
+      {/* <Posts posts={posts} /> */}
+      <FormAddPost />
+      <Posts />
     </main>
   );
 }
 
-function Posts({ posts }) {
+// function Posts({ posts }) {
+function Posts() {
   return (
     <section>
-      <List posts={posts} />
+      {/* <List posts={posts} /> */}
+      <List />
     </section>
   );
 }
 
-function FormAddPost({ onAddPost }) {
+// function FormAddPost({ onAddPost }) {
+function FormAddPost() {
+  const {onAddPost} = useContext(PostContext);
+  
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
 
@@ -166,7 +176,10 @@ function FormAddPost({ onAddPost }) {
   );
 }
 
-function List({ posts }) {
+// function List({ posts }) {
+function List() {
+  const {posts} = useContext(PostContext);
+
   return (
     <ul>
       {posts.map((post, i) => (
@@ -179,7 +192,9 @@ function List({ posts }) {
   );
 }
 
-function Archive({ onAddPost }) {
+// function Archive({ onAddPost }) {
+function Archive() {
+  const {onAddPost} = useContext(PostContext);
   // Here we don't need the setter function. We're only using state to store these posts because the callback function passed into useState (which generates the posts) is only called once, on the initial render. So we use this trick as an optimization technique, because if we just used a regular variable, these posts would be re-created on every render. We could also move the posts outside the components, but I wanted to show you this trick 😉
   const [posts] = useState(() =>
     // 💥 WARNING: This might make your computer slow! Try a smaller `length` first
