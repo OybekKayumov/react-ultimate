acbd01e1412c7127ec070e32358ce8f2cf348341
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-07-13 20:11:06 +0500

    Add Finishing the City View

--- 11-worldwise/src/components/CityItem.jsx
+++ 11-worldwise/src/components/CityItem.jsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 import styles from './CityItem.module.css';
+import { useCities } from '../contexts/CitiesContext';
 
 const formatDate = (date) =>
   new Intl.DateTimeFormat("en", {
@@ -10,11 +11,18 @@ const formatDate = (date) =>
 
 const CityItem = ({ city }) => {
   console.log('city: ', city);
+  const {currentCity} = useCities();
   const { cityName, emoji, date, id, position } = city;
 
   return (
     <li>
-      <Link  className={styles.cityItem} to={`${id}?lat=${position.lat}&lng=${position.lng}`}>
+      <Link
+        to={`${id}?lat=${position.lat}&lng=${position.lng}`}
+        className={`${styles.cityItem} ${id === currentCity.id 
+          ? styles['cityItem--active']
+          : ''
+        }`} 
+      >
         <span className={styles.emoji}>{emoji}</span>
         <h3 className={styles.name}>{cityName}</h3>
         <time className={styles.date}>({formatDate(date)})</time>
