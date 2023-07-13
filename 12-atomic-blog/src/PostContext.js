9a65b50403f4f625955991174a51135661f1a909
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-07-13 05:04:55 +0500

    Add Advanced Pattern: A Custom Provider and Hook

--- 12-atomic-blog/src/PostContext.js
+++ 12-atomic-blog/src/PostContext.js
@@ -52,6 +52,10 @@ function PostProvider({ children }) {
 
 function usePosts() {
   const context = useContext(PostContext);
+
+  if (context === undefined) {
+    throw new Error('PostContext was used outside of the PostProvider!')
+  }
   return context;
 }
 
