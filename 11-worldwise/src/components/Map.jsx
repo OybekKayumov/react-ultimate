e8019d9b8f5a00fde8e1386bf30c7dc05c6fa0c2
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-07-13 20:52:44 +0500

    Add Including a Map With the Leaflet Library

--- 11-worldwise/src/components/Map.jsx
+++ 11-worldwise/src/components/Map.jsx
@@ -30,12 +30,12 @@ const Map = () => {
       <MapContainer 
         center={mapPosition} 
         zoom={13} 
-        scrollWheelZoom={false}
+        scrollWheelZoom={true}
         className={styles.map}
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
         />
         <Marker position={mapPosition}>
           <Popup>
