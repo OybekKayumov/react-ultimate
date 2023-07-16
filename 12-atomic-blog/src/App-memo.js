95712301481b8bb110510db2ba63a835efbc14ce
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-07-17 04:51:09 +0500

    Add memo in Practice

--- 12-atomic-blog/src/App-memo.js
+++ 12-atomic-blog/src/App-memo.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { memo, useEffect, useState } from "react";
 import { faker } from "@faker-js/faker";
 
 function createRandomPost() {
@@ -155,7 +155,7 @@ function List({ posts }) {
   );
 }
 
-function Archive({ show }) {
+const Archive = memo ( function Archive({ show }) {
   // Here we don't need the setter function. We're only using state to store these posts because the callback function passed into useState (which generates the posts) is only called once, on the initial render. So we use this trick as an optimization technique, because if we just used a regular variable, these posts would be re-created on every render. We could also move the posts outside the components, but I wanted to show you this trick 😉
   const [posts] = useState(() =>
     // 💥 WARNING: This might make your computer slow! Try a smaller `length` first
@@ -185,7 +185,7 @@ function Archive({ show }) {
       )}
     </aside>
   );
-}
+})
 
 function Footer() {
   return <footer>&copy; by The Atomic Blog ✌️</footer>;
