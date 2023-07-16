22b0c2cfe023e81c89fdb59c2dabcf7a0247d486
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-07-16 05:42:27 +0500

    Adding Fake Authentication: Implementing Login

--- 11-worldwise/src/pages/Homepage.jsx
+++ 11-worldwise/src/pages/Homepage.jsx
@@ -19,7 +19,8 @@ export default function Homepage() {
           you have wandered the world.
         </h2>
 
-        <Link to='/app' className='cta'>
+        {/* <Link to='/app' className='cta'> */}
+        <Link to='/login' className='cta'>
           Start Tracking Now
         </Link>
       </section>
