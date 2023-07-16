5cf3769af3db0c92b3a9a41dc0da81f8eeb62c29
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-07-16 05:00:34 +0500

    Adding Fake Authentication: Setting Up Context

---
+++ 11-worldwise/src/contexts/FakeAuthContex.jsx
@@ -0,0 +1,59 @@
+import { createContext, useContext, useReducer } from "react";
+
+const AuthContext = createContext();
+
+const initialState = {
+  user: null,
+  isAuthenticated: false,
+}
+
+function reducer(state, action) {
+  switch(action.type) {
+    case 'login':
+      return {
+        ...state,
+        user: action.payload,
+        isAuthenticated: true
+      }
+    case 'logout':
+      return {
+        ...state,
+        user: null,
+        isAuthenticated: false
+      }
+    default:
+      throw new Error("Unknown action!")
+  }
+}
+
+function AuthProvider({ children }) {
+  const [{user, isAuthenticated}, dispatch] = useReducer(reducer, initialState)
+
+  function login(email, password) {
+    
+  }
+
+  function logout() {
+    
+  }
+
+  return (
+    <AuthContext.Provider
+      value={{
+        user,
+        isAuthenticated,
+        login,
+        logout,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+function useAuth() {
+  const context = useContext(AuthContext);
+
+  if (context === undefined)
+    throw new Error("AuthContext was used outside AuthProvider")
+}
