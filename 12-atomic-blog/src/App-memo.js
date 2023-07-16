b750d0e9c673ecd4ccf347c873101291f3b9cd2d
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-07-17 04:55:48 +0500

    Add memo in Practice

--- 12-atomic-blog/src/App-memo.js
+++ 12-atomic-blog/src/App-memo.js
@@ -41,6 +41,12 @@ function App() {
     [isFakeDark]
   );
 
+  // passing an object
+  const archiveOptions = {
+    show: false,
+    title: "Post archive in addition to main posts",
+  } 
+
   return (
     <section>
       <button
@@ -58,7 +64,8 @@ function App() {
       />
       <Main posts={searchedPosts} onAddPost={handleAddPost} />
       {/* <Archive onAddPost={handleAddPost} /> */}
-      <Archive show={false} />
+      {/* <Archive show={false} /> */}
+      <Archive archiveOptions={archiveOptions} />
       <Footer />
     </section>
   );
@@ -155,18 +162,19 @@ function List({ posts }) {
   );
 }
 
-const Archive = memo ( function Archive({ show }) {
+// const Archive = memo ( function Archive({ show }) {
+const Archive = memo ( function Archive({ archiveOptions }) {
   // Here we don't need the setter function. We're only using state to store these posts because the callback function passed into useState (which generates the posts) is only called once, on the initial render. So we use this trick as an optimization technique, because if we just used a regular variable, these posts would be re-created on every render. We could also move the posts outside the components, but I wanted to show you this trick 😉
   const [posts] = useState(() =>
     // 💥 WARNING: This might make your computer slow! Try a smaller `length` first
     Array.from({ length: 10000 }, () => createRandomPost())
   );
 
-  const [showArchive, setShowArchive] = useState(show);
+  const [showArchive, setShowArchive] = useState(archiveOptions.show);
 
   return (
     <aside>
-      <h2>Post archive</h2>
+      <h2>{archiveOptions.title}</h2>
       <button onClick={() => setShowArchive((s) => !s)}>
         {showArchive ? "Hide archive posts" : "Show archive posts"}
       </button>
