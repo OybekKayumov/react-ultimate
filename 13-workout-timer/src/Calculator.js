2200adfd65b7eb88dfec7841c650da6abecb0ce1
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-07-17 17:38:02 +0500

    Add  Using Helper Functions In Effects

--- 13-workout-timer/src/Calculator.js
+++ 13-workout-timer/src/Calculator.js
@@ -1,4 +1,4 @@
-import { memo, useEffect, useState } from 'react';
+import { memo, useCallback, useEffect, useState } from 'react';
 import clickSound from './ClickSound.m4a';
 
 function Calculator({ workouts, allowSound }) {
@@ -9,21 +9,24 @@ function Calculator({ workouts, allowSound }) {
 
   // const duration = (number * sets * speed) / 60 + (sets - 1) * durationBreak;
   const [duration, setDuration] = useState(0);
+
+  const playSound = useCallback( function () {
+    if (!allowSound) return;
+    const sound = new Audio(clickSound);
+    sound.play();
+  }, [allowSound]);
+
   useEffect(() => {
     setDuration(
       (number * sets * speed) / 60 + (sets - 1) * durationBreak
     );
-  }, [number, sets, speed, durationBreak]);
+
+    playSound();
+  }, [number, sets, speed, durationBreak, playSound]);
 
   const mins = Math.floor(duration);
   const seconds = (duration - mins) * 60;
-
-  const playSound = function () {
-    if (!allowSound) return;
-    const sound = new Audio(clickSound);
-    sound.play();
-  };
-
+  
   function handleInc() {
     setDuration(duration => Math.floor(duration) + 1);
     playSound();
