c37dff39734c011246cd4a18bc12f3670aa492b4
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-07-18 12:59:45 +0500

    Add Connecting our Redux App With React

--- 14-redux-intro/src/features/customers/Customer.js
+++ 14-redux-intro/src/features/customers/Customer.js
@@ -1,4 +1,14 @@
+import { useSelector } from "react-redux";
+
 function Customer() {
+  //! read data from the Redux store - store.js
+  // const rootReducer = combineReducers({
+  //   account: accountReducer,
+  //!   customer: customerReducer,
+  // });
+  const customer = useSelector(store => store.customer);
+  console.log('from customer component: ', customer);
+
   return <h2>👋 Welcome, %NAME%</h2>;
 }
 
--- 14-redux-intro/src/index.js
+++ 14-redux-intro/src/index.js
@@ -19,3 +19,9 @@ root.render(
 // import './store';
 // store.dispatch({ type: 'account/deposit', payload: 250 });
 // console.log(': ', store.getState());
+
+// now our application knows about the Redux store
+// every single component in the application can now read data from the store and can dispatch actions to it.
+
+// that is very similar to the behavior that we see in the Context API.
+// it's basically broadcasting global state into every component that actually wants to read it.
