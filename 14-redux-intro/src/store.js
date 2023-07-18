59d959ecb706f738ac7cd8a69e04dd55b1d4d1c3
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-07-19 04:52:45 +0500

    Add The Redux DevTools

--- 14-redux-intro/src/store.js
+++ 14-redux-intro/src/store.js
@@ -1,5 +1,7 @@
 import { applyMiddleware, combineReducers, createStore } from 'redux';
 import thunk from 'redux-thunk';
+import {composeWithDevTools} from 'redux-devtools-extension';
+
 import accountReducer from './features/accounts/accountSlice';
 import customerReducer from './features/customers/customerSlice';
 
@@ -8,6 +10,9 @@ const rootReducer = combineReducers({
   customer: customerReducer,
 });
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 export default store;
