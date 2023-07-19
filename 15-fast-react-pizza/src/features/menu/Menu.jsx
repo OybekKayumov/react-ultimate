2ec7627ea7dfbc09e3e895778596f1b5f3edad50
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-07-20 04:59:36 +0500

    Add Fetching Data With React Router Loaders: Pizza Menu

--- 15-fast-react-pizza/src/features/menu/Menu.jsx
+++ 15-fast-react-pizza/src/features/menu/Menu.jsx
@@ -1,5 +1,14 @@
+import { getMenu } from "../../services/apiRestaurant";
+
 function Menu() {
   return <h1>Menu</h1>;
 }
+
+// create a loader fn --> fetch data and return it
+function loader() {
+  const menu = await getMenu();
+  return menu;
+}
+
 
 export default Menu;
