749612deb1d98e43d961a48c17e572621b460be3
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-07-19 14:10:20 +0500

    Add Back to Thunks

--- 14-redux-intro/src/features/accounts/accountSlice.js
+++ 14-redux-intro/src/features/accounts/accountSlice.js
@@ -15,28 +15,56 @@ const accountSlice = createSlice({
     deposit(state, action) {
       // state.balance = state.balance + action.payload;
       state.balance += action.payload;
+      state.isLoading = false;
     },
     withdraw(state,action) {
       state.balance -= action.payload; 
     },
-    requestLoan(state, action) {
+    requestLoan: {
+      prepare(amount, purpose) {
+        return {
+          payload: { amount, purpose }
+        }
+      },
+
+      reducer (state, action) {
       if (state.loan > 0) return;
 
       state.loan = action.payload.amount;
       state.loanPurpose = action.payload.purpose;
       state.balance = state.balance + action.payload.amount;
-    },
-    payLoan(state, action) {
+    }},
+    payLoan(state) {
+      state.balance -= state.loan;
       state.loan = 0;
       state.loanPurpose = '';
-      state.balance -= state.loan;
     },
+    convertingCurrency(state) {
+      state.isLoading = true
+    }
   },
 });
 
-console.log('ac: ', accountSlice);
+// console.log('ac: ', accountSlice);
 
-export const { deposit, withdraw, requestLoan, payLoan} = accountSlice.actions;
+export const { withdraw, requestLoan, payLoan} = accountSlice.actions;
+
+export function deposit(amount, currency) {
+  if (currency === 'USD') return { type: "account/deposit", payload: amount }
+
+  // if not USD, convert fn
+  return async function (dispatch, getState) {
+    dispatch({ type: "account/convertingCurrency" });
+
+    const res = await fetch(
+      `https://api.frankfurter.app/latest?amount=${amount}&from=${currency}&to=USD`
+    );
+    const data = await res.json();
+    const converted = data.rates.USD;
+
+    dispatch({ type: "account/deposit", payload: converted });
+  };
+}
 
 export default accountSlice.reducer;
 
