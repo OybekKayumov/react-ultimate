5840c45161f87a72110173673bff4a8aab4d25c2
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-07-19 17:09:14 +0500

    Add A New Way Of Implementing Routes

--- 15-fast-react-pizza/src/App.jsx
+++ 15-fast-react-pizza/src/App.jsx
@@ -1,9 +1,22 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: '/menu',
+    element: <Menu />
+  }
+]) 
 
 function App() {
   return (
-    <>
-      hello
-    </>
+    <RouterProvider router={router} />
   )
 }
 
