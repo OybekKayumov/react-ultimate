dfcf3ce9c93858fb6674895a1bd3e1db9ef283a8
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-07-19 05:30:15 +0500

    Add Creating the Account Slice

--- 14-redux-intro/src/features/accounts/accountSlice.js
+++ 14-redux-intro/src/features/accounts/accountSlice.js
@@ -18,9 +18,27 @@ const accountSlice = createSlice({
     },
     withdraw(state,action) {
       state.balance -= action.payload; 
-    }
-  }
-})
+    },
+    requestLoan(state, action) {
+      if (state.loan > 0) return;
+
+      state.loan = action.payload.amount;
+      state.loanPurpose = action.payload.purpose;
+      state.balance = state.balance + action.payload.amount;
+    },
+    payLoan(state, action) {
+      state.loan = 0;
+      state.loanPurpose = '';
+      state.balance -= state.loan;
+    },
+  },
+});
+
+console.log('ac: ', accountSlice);
+
+export const { deposit, withdraw, requestLoan, payLoan} = accountSlice.actions;
+
+export default accountSlice.reducer;
 
 /*
 export default function accountReducer(state = initialStateAccount, action) {
