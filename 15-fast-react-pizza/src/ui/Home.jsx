e6f2cc5b4cbd3adbba0b1197e71be5493372767b
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-07-20 16:58:41 +0500

    Add Working With Colo

--- 15-fast-react-pizza/src/ui/Home.jsx
+++ 15-fast-react-pizza/src/ui/Home.jsx
@@ -1,7 +1,7 @@
 function Home() {
   return (
     <div>
-      <h1 className="text-xl text-yellow-500 font-semibold text-center">
+      <h1 className="text-xl text-fuchsia-500 font-semibold text-center">
         The best pizza.
         <br />
         Straight out of the oven, straight to you.
