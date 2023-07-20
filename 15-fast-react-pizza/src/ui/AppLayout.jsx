ca5344da917494338fb31b290b7a9d58844bd2b1
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-07-20 05:21:57 +0500

    Add Displaying a Loading Indicator

--- 15-fast-react-pizza/src/ui/AppLayout.jsx
+++ 15-fast-react-pizza/src/ui/AppLayout.jsx
@@ -1,13 +1,18 @@
 import { Outlet, useNavigation } from 'react-router-dom';
 import Header from './Header';
 import CartOverview from '../features/cart/CartOverview';
+import Loader from './Loader';
 
 const AppLayout = () => {
   const navigation = useNavigation();
   console.log(': ', navigation);
-  
+  const isLoading = navigation.state === "loading";
+
+
   return (
-    <div>
+    <div className='layout'>
+      {isLoading && <Loader />}
+      
       <Header />
 
       <main>
