7a7b8cb4809d5d200e5da4d721afb8da9930143a
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-07-20 05:24:50 +0500

    Add Handling Errors With Error Elements

--- 15-fast-react-pizza/src/App.jsx
+++ 15-fast-react-pizza/src/App.jsx
@@ -1,6 +1,7 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import Home from "./ui/Home";
+import Error from "./ui/Error";
 import Menu, {loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import Order from "./features/order/Order";
@@ -10,6 +11,7 @@ import AppLayout from "./ui/AppLayout";
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
+    errorElement: <Error />,
     children: [
       { path: '/', element: <Home /> },
       { path: '/menu', element: <Menu />, loader: menuLoader },
