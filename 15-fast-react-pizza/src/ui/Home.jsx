e18f57ae0d3140c19544d9265c432355921a113d
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-07-21 04:51:52 +0500

    Add Responsive Design

--- 15-fast-react-pizza/src/ui/Home.jsx
+++ 15-fast-react-pizza/src/ui/Home.jsx
@@ -3,7 +3,7 @@ import CreateUser from "../features/user/CreateUser";
 function Home() {
   return (
     // mb-10 mt-10 = my-10
-    <div className="my-10 text-center"> 
+    <div className="my-10 sm:my-16 text-center"> 
       <h1 className="text-xl font-semibold mb-8">
         The best pizza.
         <br />
