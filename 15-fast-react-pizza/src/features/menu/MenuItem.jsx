9c0eef5eb2e417568881c3735dfb3be6887663bd
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-07-21 18:02:52 +0500

    Add Styling the Menu

--- 15-fast-react-pizza/src/features/menu/MenuItem.jsx
+++ 15-fast-react-pizza/src/features/menu/MenuItem.jsx
@@ -6,13 +6,19 @@ function MenuItem({ pizza }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
 
   return (
-    <li>
-      <img src={imageUrl} alt={name} />
-      <div>
+    <li className="flex gap-4 py-2 ">
+      <img 
+        src={imageUrl} 
+        alt={name} 
+        className={`h-24 ${soldOut ? "opacity-70 grayscale" : ""}`}
+      />
+      <div className="flex flex-col">
         <p>{name}</p>
         <p>{ingredients.join(', ')}</p>
-        <div>
-          {!soldOut ? <p>{formatCurrency(unitPrice)}</p> : <p>Sold out</p>}
+        <div className="mt-auto">
+          {!soldOut 
+            ? <p className="text-sm">{formatCurrency(unitPrice)}</p> 
+            : <p className="text-sm uppercase font-medium text-stone-500">Sold out</p>}
         </div>
       </div>
     </li>
