cc384f72241290312d093debd634fbc728785c9f
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-07-21 16:26:49 +0500

    Add Absolute Positioning, z-index, and More

--- 15-fast-react-pizza/src/ui/AppLayout.jsx
+++ 15-fast-react-pizza/src/ui/AppLayout.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import { Outlet, useNavigation } from 'react-router-dom';
 import Header from './Header';
 import CartOverview from '../features/cart/CartOverview';
--- 15-fast-react-pizza/src/ui/Loader.jsx
+++ 15-fast-react-pizza/src/ui/Loader.jsx
@@ -1,8 +1,12 @@
 
 const Loader = () => {
   return (
-    <div className='loader'>
+    <div 
+      className="absolute bg-slate-300/30 inset-0 backdrop-blur-sm flex items-center justify-center"
+    >
+      <div className='loader'>
 
+      </div>
     </div>
   )
 }
