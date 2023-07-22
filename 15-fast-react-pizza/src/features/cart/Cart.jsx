ab995515e53b91e54b4449a45a03fc85d5a457d1
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-07-22 13:55:35 +0500

    Add Reading and Updating the User State

--- 15-fast-react-pizza/src/features/cart/Cart.jsx
+++ 15-fast-react-pizza/src/features/cart/Cart.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import LinkButton from '../../ui/LinkButton';
 import Button from '../../ui/Button';
 import CartItem from './CartItem';
+import { useSelector } from 'react-redux';
 
 const fakeCart = [
   {
@@ -29,6 +30,7 @@ const fakeCart = [
 ];
 
 function Cart() {
+  const username = useSelector(state => state.user.username);
   const cart = fakeCart;
 
   return (
@@ -39,7 +41,7 @@ function Cart() {
         &larr; Back to menu
       </LinkButton>
 
-      <h2 className='mt-7 text-xl font-semibold'>Your cart, %NAME%</h2>
+      <h2 className='mt-7 text-xl font-semibold'>Your cart, {username}</h2>
 
       <ul className='divide-y divide-stone-200 border-b mt-3'>
         {cart.map(item => (
--- 15-fast-react-pizza/src/features/order/CreateOrder.jsx
+++ 15-fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
 import Button from "../../ui/Button";
+import { useSelector } from "react-redux";
 
 // https://uibakery.io/regex-library/phone-number
 // eslint-disable-next-line no-unused-vars
@@ -37,6 +38,7 @@ const fakeCart = [
 ];
 
 function CreateOrder() {
+  const username = useSelector(state => state.user.username);
   const navigation = useNavigation();
   const isSubmitting = navigation.state === 'submitting';
 
@@ -54,7 +56,14 @@ function CreateOrder() {
       <Form method="POST">
         <div className="mb-5 flex gap-2 flex-col sm:flex-row sm:items-center">
           <label className="sm:basis-40">First Name</label>
-          <input className="input grow" type="text" name="customer" required />
+          <input 
+            // defaultValue -> it's a normal HTML elem, not a controlled element
+            defaultValue={username}  
+            className="input grow" 
+            type="text" 
+            name="customer" 
+            required 
+          />
         </div>
 
         <div className="mb-5 flex gap-2 flex-col sm:flex-row sm:items-center">
