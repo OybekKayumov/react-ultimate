c8aff81623711060135b41f35ecf210dd8e37ac9
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-07-22 16:27:57 +0500

    Add Building the Cart Overview With Redux Selectors

--- 15-fast-react-pizza/src/features/cart/CartOverview.jsx
+++ 15-fast-react-pizza/src/features/cart/CartOverview.jsx
@@ -1,15 +1,21 @@
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { getTotalCartPrice, getTotalCartQuantity } from "./cartSlice";
+import { formatCurrency } from "../../utils/helpers";
 
 function CartOverview() {
   // read state from redux store
-  const totalCartQuantity = useSelector(state => state.cart.cart.reduce((sum, item) => sum + item.quantity,0));
+  // const totalCartQuantity = useSelector(state => state.cart.cart.reduce((sum, item) => sum + item.quantity,0));
+  const totalCartQuantity = useSelector(getTotalCartQuantity);
+  const totalCartPrice = useSelector(getTotalCartPrice);
+
+  if (!totalCartQuantity) return null;
 
   return (
     <div className="bg-stone-700 text-stone-200 uppercase px-4 py-4 sm:px-6 text-sm md:text-base flex items-center justify-between">
       <p className="text-stone-300 font-semibold space-x-4 sm:space-x-6">
         <span>{totalCartQuantity} pizzas</span>
-        <span>$23.45</span>
+        <span>{formatCurrency(totalCartPrice)}</span>
       </p>
       <Link to="/cart">Open cart &rarr;</Link>
     </div>
