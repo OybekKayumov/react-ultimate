ba913a3160935a4dc6f3680850ab8610e3e498f9
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-07-24 04:57:23 +0500

    Add Using the Cart for New Orders

--- 15-fast-react-pizza/src/features/order/CreateOrder.jsx
+++ 15-fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -6,7 +6,8 @@ import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
 import Button from "../../ui/Button";
 import EmptyCart from "../cart/EmptyCart";
-import {getCart} from "../cart/cartSlice.js"
+import {clearCart, getCart} from "../cart/cartSlice.js";
+import store from "../../store.js";
 
 // https://uibakery.io/regex-library/phone-number
 // eslint-disable-next-line no-unused-vars
@@ -146,6 +147,9 @@ export async function action({ request }) {
 
   // if everything OK, create new Order and redirect
   const newOrder = await createOrder(order);
+
+  // ! do not overuse
+  store.dispatch(clearCart());
 
   return redirect(`/order/${newOrder.id}`);
 }
