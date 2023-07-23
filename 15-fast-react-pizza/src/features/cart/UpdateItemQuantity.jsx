c5fa0487e5fb83a9f9ea935bba371d9a3a268b2e
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-07-23 05:12:28 +0500

    Add Updating Cart Quantities

---
+++ 15-fast-react-pizza/src/features/cart/UpdateItemQuantity.jsx
@@ -0,0 +1,12 @@
+import Button from "../../ui/Button";
+
+const UpdateItemQuantity = ({ pizzaId }) => {
+  return (
+    <div>
+      <Button type="round">-</Button>
+      <Button type="round">+</Button>
+    </div>
+  )
+}
+
+export default UpdateItemQuantity
