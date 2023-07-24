de0c503d0ab969acf9538eb841df9038b9019dfa
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-07-24 15:15:46 +0500

    Add Updating Data Without Navigation

--- 15-fast-react-pizza/src/features/order/UpdateOrder.jsx
+++ 15-fast-react-pizza/src/features/order/UpdateOrder.jsx
@@ -1,11 +1,29 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import { useFetcher } from "react-router-dom";
 import Button from "../../ui/Button";
+import { updateOrder } from "../../services/apiRestaurant";
 
 const UpdateOrder = ({order}) => {
+  const fetcher = useFetcher();
+
   return (
-    <Button type="primary">
-      Make Priority
-    </Button>
+    <fetcher.Form method="PATCH" className="text-right">
+      <Button type="primary">
+        Make Priority
+      </Button>
+    </fetcher.Form>
   )
+}
+
+export async function action({request, params }) {
+  // console.log('update: ', );
+
+  const data = {priority : true };
+
+  await updateOrder(params.orderId, data);
+
+  return null;
 }
 
 export default UpdateOrder
