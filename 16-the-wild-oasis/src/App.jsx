8fe5584d7b0cfdebb9a277c2d93eb46615a613df
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-07-25 05:35:29 +0500

    Add Building More Reusable Styled Components

--- 16-the-wild-oasis/src/App.jsx
+++ 16-the-wild-oasis/src/App.jsx
@@ -3,9 +3,10 @@ import GlobalStyles from "./styles/GlobalStyle";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
 import Heading from "./ui/Heading";
+import Row from "./ui/Row";
 
 const StyledApp = styled.div`
-  background-color: orange;
+  /* background-color: orange; */
   padding: 20px;
 `;
 
@@ -16,13 +17,23 @@ function App() {
     <>
     <GlobalStyles />
     <StyledApp>
-      <Heading as='h1'>The Wild Oasis</Heading>
+      <Row type='horizontal'>
+        <Heading as='h1'>The Wild Oasis</Heading>
+        <div>
+          <Heading as='h2'>Check In and Out</Heading>
+          <Button>Check In</Button>
+          <Button>Check Out</Button>
+        </div>
+      </Row>
 
-      <Heading as='h2'>Check In and Out</Heading>
-      <Button>Check In</Button>
-      <Input type="number" placeholder="Number of guests"/>
+      <Row type='vertical'>
+        <Heading as='h3'>Form</Heading>
+        <form action="">
+          <Input type="number" placeholder="Number of guests"/>
+          <Input type="number" placeholder="Number of guests"/>
+        </form>
+      </Row>
 
-      <Heading as='h3'>Form</Heading>
     </StyledApp>
     </>
   )
