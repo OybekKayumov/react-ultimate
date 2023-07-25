c00855d1c2c60977c16ba6b714eb4a145f3c6cb7
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-07-25 14:00:51 +0500

    Add Building the Sidebar and Main Navigation

--- 16-the-wild-oasis/src/ui/MainNav.jsx
+++ 16-the-wild-oasis/src/ui/MainNav.jsx
@@ -2,7 +2,7 @@
 import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 
-import { HiOutlineCalendarDays, HiOutlineHome } from "react-icons/hi2";
+import { HiOutlineCalendarDays, HiOutlineCog6Tooth, HiOutlineHome, HiOutlineHomeModern, HiOutlineUsers } from "react-icons/hi2";
 
 const NavList = styled.ul`
   display: flex;
@@ -68,6 +68,26 @@ function MainNav() {
           </StyledNavLink>
         </li>
 
+        <li>
+          <StyledNavLink to="/cabins">
+            <HiOutlineHomeModern />
+            <span>Cabins</span>
+          </StyledNavLink>
+        </li>
+
+        <li>
+          <StyledNavLink to="/users">
+            <HiOutlineUsers />
+            <span>Users</span>
+          </StyledNavLink>
+        </li>
+
+        <li>
+          <StyledNavLink to="/settings">
+            <HiOutlineCog6Tooth />
+            <span>Settings</span>
+          </StyledNavLink>
+        </li>
       </NavList>
     </nav>
   )
--- 16-the-wild-oasis/src/ui/Sidebar.jsx
+++ 16-the-wild-oasis/src/ui/Sidebar.jsx
@@ -8,6 +8,10 @@ const StyledSidebar = styled.aside`
   border-bottom: 1px solid var(--color-grey-100);
 
   grid-row: 1 / -1;
+
+  display: flex;
+  flex-direction: column;
+  gap: 3.2rem;
 `;
 
 const Sidebar = () => {
