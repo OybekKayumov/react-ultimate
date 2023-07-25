53eb64cde5f7be0786a25c61b9b0c0a6985dba0e
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-07-25 05:02:45 +0500

    Add Global Styles With Styled Components

--- 16-the-wild-oasis/src/App.jsx
+++ 16-the-wild-oasis/src/App.jsx
@@ -12,17 +12,20 @@ const Button = styled.button`
   padding: 1.2rem 1.6rem;
   font-weight: 500;
   border: none;
-  border-radius: 7px;
-  background-color: purple;
-  color: white;
+  border-radius: var(--border-radius-sm);
+  background-color: var(--color-brand-600);
+  color: var(--color-brand-50);
+  box-shadow: var(--shadow-sm);
   cursor: pointer;
-  margin: 20px;
+  /* margin: 20px; */
 `;
 
 const Input = styled.input`
-  border: 1px solid #ddd;
-  border-radius: 5px;
+  border: 1px solid var(--color-grey-300);
+  background-color: var(--color-grey-0);
+  border-radius: var(--border-radius-sm);
   padding: 0.8rem 1.2rem;
+  box-shadow: var(--shadow-sm);
 `;
 
 const StyledApp = styled.div`
