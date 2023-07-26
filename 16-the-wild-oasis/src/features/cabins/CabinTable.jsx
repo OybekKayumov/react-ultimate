a31b20d466266101cef17316417bbe859eb0c2bc
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-07-26 15:55:51 +0500

    Add Fetching Cabin Data

--- 16-the-wild-oasis/src/features/cabins/CabinTable.jsx
+++ 16-the-wild-oasis/src/features/cabins/CabinTable.jsx
@@ -1,5 +1,9 @@
 /* eslint-disable no-unused-vars */
+import { useQuery } from "@tanstack/react-query";
 import styled from "styled-components";
+import { getCabins } from "../../services/apiCabins";
+import Spinner from "../../ui/Spinner";
+import CabinRow from "./CabinRow";
 
 const Table = styled.div`
   border: 1px solid var(--color-grey-200);
@@ -26,8 +30,28 @@ const TableHeader = styled.header`
 `;
 
 const CabinTable = () => {
+  const {isLoading, data: cabins, error} = useQuery({
+    queryKey: ['cabin'],
+    queryFn: getCabins,
+  });
+
+  if (isLoading) return <Spinner />
+
   return (
-    <div>CabinTable</div>
+    <Table role='table'>
+      <TableHeader role='row'>
+        <div></div>
+        <div>Cabin</div>
+        <div>Capacity</div>
+        <div>Price</div>
+        <div>Discount</div>
+        <div></div>
+      </TableHeader>
+
+      {cabins.map(cabin => (
+        <CabinRow cabin={cabin} key={cabin.id}/>
+      ))}
+    </Table>
   )
 }
 
