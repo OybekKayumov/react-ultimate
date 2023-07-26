7f41a0ffcdb887f1d72bfc9a824ebd09b0c579b7
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-07-26 15:27:59 +0500

    Add import React from 'react'

--- 16-the-wild-oasis/src/features/cabins/CabinTable.jsx
+++ 16-the-wild-oasis/src/features/cabins/CabinTable.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import styled from "styled-components";
 
 const Table = styled.div`
@@ -23,3 +24,11 @@ const TableHeader = styled.header`
   color: var(--color-grey-600);
   padding: 1.6rem 2.4rem;
 `;
+
+const CabinTable = () => {
+  return (
+    <div>CabinTable</div>
+  )
+}
+
+export default CabinTable
--- 16-the-wild-oasis/src/pages/Cabins.jsx
+++ 16-the-wild-oasis/src/pages/Cabins.jsx
@@ -1,22 +1,30 @@
+/* eslint-disable no-unused-vars */
 import { useEffect } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import { getCabins } from "../services/apiCabins";
+import CabinTable from "../features/cabins/CabinTable";
 
 function Cabins() {
 
   // get cabins from supabase
-  useEffect(function () {
-    getCabins().then(data => console.log('cabins data: ', data ))
-  }, []);
+  // useEffect(function () {
+  //   getCabins().then(data => console.log('cabins data: ', data ))
+  // }, []);
 
   return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">All cabins</Heading>
+        <p>TEST</p>
 
-      <img src="https://ffglytqkuxpsyimghhhi.supabase.co/storage/v1/object/public/cabin-omages/cabin-001.jpg" alt="cabin img" />
-    </Row>
+        {/* <img src="https://ffglytqkuxpsyimghhhi.supabase.co/storage/v1/object/?public/cabin-omages/cabin-001.jpg" alt="cabin img" /> */}
+      </Row>
+
+      <Row>
+        <CabinTable />
+      </Row>
+    </>
   );
 }
 
