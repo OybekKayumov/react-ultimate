4e9bead6e65b1a5206dd3f420f56a1fbab285a28
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-07-27 05:01:27 +0500

    Add Displaying Toasts (Notifications)

--- 16-the-wild-oasis/src/App.jsx
+++ 16-the-wild-oasis/src/App.jsx
@@ -9,6 +9,7 @@ import styled from "styled-components";
 import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
+import { Toaster } from "react-hot-toast";
 
 import GlobalStyles from "./styles/GlobalStyle";
 import Dashboard from "./pages/Dashboard";
@@ -57,6 +58,27 @@ function App() {
 
         </Routes>
       </BrowserRouter>
+
+      <Toaster 
+        position="top-center" 
+        gutter={12} 
+        containerStyle={{margin: "8px"}} 
+        toastOptions={{
+          success: {
+            duration: 3000,
+          },
+          error: {
+            duration: 3000,
+          },
+          style: {
+            fontSize: "16px",
+            maxWidth: "500px",
+            padding: "16px 24px",
+            backgroundColor: "var(--color--grey-0",
+            color: "var(--color--grey-700",
+          }
+        }}
+      />
     </QueryClientProvider>
   )
 }
--- 16-the-wild-oasis/src/features/cabins/CabinRow.jsx
+++ 16-the-wild-oasis/src/features/cabins/CabinRow.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
+import { toast } from "react-hot-toast";
 import styled from "styled-components";
 import { formatCurrency } from "../../utils/helpers.js";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
@@ -61,13 +62,13 @@ function CabinRow({cabin}) {
     // mutationFn: (id) => deleteCabin(id),
     mutationFn: deleteCabin,
     onSuccess: () => {
-      alert("Deleted")
+      toast.success("Cabin successfully deleted!")
       queryClient.invalidateQueries({
         queryKey: ["cabins"],
       });
     },
 
-    onError: err => alert(err.message),
+    onError: err => toast.error(err.message),
   });
 
   return (
