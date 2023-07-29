942a67fb7d16d0e385fb2b24cf4a0b4bc348e534
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-07-29 11:40:34 +0500

    Add Building the Bookings Table

--- 16-the-wild-oasis/src/services/apiBookings.js
+++ 16-the-wild-oasis/src/services/apiBookings.js
@@ -1,10 +1,11 @@
+/* eslint-disable no-unused-vars */
 import { getToday } from "../utils/helpers";
 import supabase from "./supabase";
 
 export async function getBookings(id) {
   const { data, error } = await supabase
     .from("bookings")
-    .select("*");
+    .select("*, cabins(*), guests(*)");
 
   if (error) {
     console.error(error);
--- 16-the-wild-oasis/src/ui/Empty.jsx
+++ 16-the-wild-oasis/src/ui/Empty.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
-function Empty({ resource }) {
-  return <p>No {resource} could be found.</p>;
+function Empty({ resourceName }) {
+  return <p>No {resourceName} could be found.</p>;
 }
 
 export default Empty;
