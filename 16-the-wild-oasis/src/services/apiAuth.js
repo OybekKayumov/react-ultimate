c5bbd043b8e650bfaeb85e8c81f854660d8f741b
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-07-30 12:13:26 +0500

    Add Authentication: User Login With Supabase

--- 16-the-wild-oasis/src/services/apiAuth.js
+++ 16-the-wild-oasis/src/services/apiAuth.js
@@ -1,6 +1,6 @@
 import supabase from "./supabase";
 
-export async function Login({email, password}) {
+export async function login({email, password}) {
   
   const { data, error } = await supabase.auth.signInWithPassword({
     // email: 'someone@email.com',
